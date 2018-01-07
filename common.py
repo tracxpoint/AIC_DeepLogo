d381dfa858a76fbf1f69cb77181031deb6e3c6b4
Hussam <hussam.lawen@tracxpoint.com>
2018-01-07 15:27:19 +0200

    change classes + dataset Paths

--- common.py
+++ common.py
@@ -30,21 +30,42 @@ __all__ = ('CLASS_NAME', 'CNN_IN_WIDTH', 'CNN_IN_HEIGHT', 'CNN_IN_CH',
            'CROPPED_AUG_IMAGE_DIR', 'ANNOT_FILE', 'ANNOT_FILE_WITH_BG')
 
 CLASS_NAME = [
-    'Adidas', 'Apple', 'BMW', 'Citroen', 'Cocacola', 'DHL', 'Fedex', 'Ferrari',
-    'Ford', 'Google', 'HP', 'Heineken', 'Intel', 'McDonalds', 'Mini', 'Nbc',
-    'Nike', 'Pepsi', 'Porsche', 'Puma', 'RedBull', 'Sprite', 'Starbucks',
-    'Texaco', 'Unicef', 'Vodafone', 'Yahoo', 'Background'
-]
+    'becks',
+    'carlsberg',
+    'cocacola',
+    'colgate',
+    'corona',
+    'danone',
+    'erdinger',
+    'fosters',
+    'fritolay',
+    'gillette',
+    'guiness',
+    'heineken',
+    'hershey',
+    'kelloggs',
+    'kraft',
+    'milka', 
+    'millerhighlife',
+    'nescafe',
+    'nestle',
+    'pampers',
+    'paulaner',
+    'pepsi',
+    'rittersport',
+    'stellaartois',
+    'tsingtao',
+    'Background'
+    ]
 
 CNN_IN_WIDTH = 64
 CNN_IN_HEIGHT = 32
 CNN_IN_CH = 3
 CNN_SHAPE = (CNN_IN_HEIGHT, CNN_IN_WIDTH, CNN_IN_CH)
 
-TRAIN_DIR = 'flickr_logos_27_dataset'
-TRAIN_IMAGE_DIR = os.path.join(TRAIN_DIR, 'flickr_logos_27_dataset_images')
-CROPPED_AUG_IMAGE_DIR = os.path.join(
-    TRAIN_DIR, 'flickr_logos_27_dataset_cropped_augmented_images')
+TRAIN_DIR = os.getenv('RETAIL25_PATH', '../Retail25')
+TRAIN_IMAGE_DIR = os.path.join(TRAIN_DIR, 'Images')
+CROPPED_AUG_IMAGE_DIR = 'cropped_augmented_images'
 ANNOT_FILE = os.path.join(
-    TRAIN_DIR, 'flickr_logos_27_dataset_training_set_annotation.txt')
+    TRAIN_DIR, 'training_data_flat.txt')
 ANNOT_FILE_WITH_BG = os.path.join(TRAIN_DIR, 'train_annot_with_bg_class.txt')
