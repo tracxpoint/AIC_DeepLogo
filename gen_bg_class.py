40259d80fa13218fa78530e43398bf5363d429d8
hussam-tracxpoint <hussam.lawen@tracxpoint.com>
2018-01-21 17:08:42 +0200

    resolve background selective search issue

--- gen_bg_class.py
+++ gen_bg_class.py
@@ -29,15 +29,22 @@ import common
 import util
 import skimage.io
 import os
+import random
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 
-def get_bg_proposals(object_proposals, annot):
-    annot_rect = util.get_annot_rect(annot)
+def get_bg_proposals(object_proposals, img_instances_annot):
 
+    temp_proposals = []
     bg_proposals = []
     for obj_proposal in object_proposals:
-        if util.iou(obj_proposal, annot_rect) <= 0.5:
+        global_bg = True
+        for annot in img_instances_annot:
+            annot_rect = util.get_annot_rect(annot)
+            if util.iou(obj_proposal, annot_rect) > 0.5:
+                global_bg = False
+                break
+        if global_bg:
             bg_proposals.append(obj_proposal)
     return bg_proposals
 
@@ -48,15 +55,17 @@ def gen_annot_file_line(img_fn, class_name, train_subset_class, rect):
     return line
 
 
-def gen_annot_file_lines(annot):
+def gen_annot_file_lines(img_instances_annot):
     lines = []
 
+    img_fn, class_name, train_subset_class = util.parse_annot(img_instances_annot[0])
     # Get original annot line
-    img_fn, class_name, train_subset_class = util.parse_annot(annot)
-    annot_rect = util.get_annot_rect(annot)
-    lines.append(
-        gen_annot_file_line(img_fn, class_name, train_subset_class,
-                            annot_rect))
+    # for annot in img_instances_annot:
+    #     img_fn, class_name, train_subset_class = util.parse_annot(annot)
+    #     annot_rect = util.get_annot_rect(annot)
+    #     lines.append(
+    #         gen_annot_file_line(img_fn, class_name, train_subset_class,
+    #                             annot_rect))
 
     # Load image
     img = skimage.io.imread(os.path.join(common.TRAIN_IMAGE_DIR, img_fn))
@@ -67,18 +76,27 @@ def gen_annot_file_lines(annot):
         return lines
 
     # Background proposals
-    bg_proposals = get_bg_proposals(object_proposals, annot)
+    bg_proposals = get_bg_proposals(object_proposals, img_instances_annot)
     if len(bg_proposals) == 0:
         return lines
 
-    # Select bg proposal
-    bg_proposal = bg_proposals[np.random.choice(
-        np.array(bg_proposals).shape[0])]
-    x1, y1, x2, y2 = bg_proposal[0], bg_proposal[
-        1], bg_proposal[0] + bg_proposal[2], bg_proposal[1] + bg_proposal[3]
-    lines.append(
-        gen_annot_file_line(img_fn, common.CLASS_NAME[-1], train_subset_class,
-                            [x1, y1, x2, y2]))
+    randomNumbers = random.sample(range(len(bg_proposals)), len(img_instances_annot))
+
+    for index, rndNum in enumerate(randomNumbers):
+        annot = img_instances_annot[index]
+        # Select bg proposal
+
+        bg_proposal = bg_proposals[rndNum]
+        x1, y1, x2, y2 = bg_proposal[0], bg_proposal[1], bg_proposal[0] + bg_proposal[2], bg_proposal[1] + bg_proposal[3]
+
+        img_fn, class_name, train_subset_class = util.parse_annot(annot)
+        annot_rect = util.get_annot_rect(annot)
+        lines.append(
+            gen_annot_file_line(img_fn, class_name, train_subset_class,
+                                annot_rect))
+        lines.append(
+            gen_annot_file_line(img_fn, common.CLASS_NAME[-1], train_subset_class,
+                                [x1, y1, x2, y2]))
 
     return lines
 
@@ -92,10 +110,27 @@ def main():
     results = []
     n_workers = os.cpu_count()
 
+    temp_img, _, _ = util.parse_annot(annot_train[0])
+    img_instances_annot = []
     with ProcessPoolExecutor(n_workers) as executer, open(
             common.ANNOT_FILE_WITH_BG, 'w') as fw:
-        for annot in annot_train:
-            results.append(executer.submit(gen_annot_file_lines, annot))
+        for idx, annot in enumerate(annot_train):
+            img_fn, class_name, train_subset_class = util.parse_annot(annot)
+
+            # new image
+            if img_fn != temp_img:
+                results.append(executer.submit(gen_annot_file_lines, img_instances_annot))
+                img_instances_annot = []
+                img_instances_annot.append(annot)
+            else:
+                img_instances_annot.append(annot)
+
+            temp_img = img_fn
+            temp_annot = annot
+        # last image
+        results.append(executer.submit(gen_annot_file_lines, img_instances_annot))
+        # for annot in annot_train:
+        #     results.append(executer.submit(gen_annot_file_lines, annot))
 
         for result in as_completed(results):
             print('\n'.join(result.result()))
